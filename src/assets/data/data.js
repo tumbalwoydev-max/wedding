8ff22b72459ec4ebea346d7566d65806f88880c1
irsanwoy <irsanwoy54@gmail.com>
2025-09-04 18:29:36 +0700

    Initial commit

---
+++ src/assets/data/data.js
@@ -0,0 +1,147 @@
+export const data = {
+  bride: {
+    L: {
+      id: 1,
+      name: "Muhammad Nuh Abdul Hamid",
+      child: "Anak ke Dua",
+      father: "Iwan",
+      mother: "Dian",
+      image: "./src/assets/images/cowo.png",
+    },
+    P: {
+      id: 2,
+      name: "Ajeng Widia Sari",
+      child: "Anak ke Tiga",
+      father: "H. Abad Muttaqin alm",
+      mother: "Ida Laela Sari",
+      image: "./src/assets/images/cewe.png",
+    },
+
+    couple: "./src/assets/images/couple.png",
+  },
+
+  time: {
+    marriage: {
+      year: "2025",
+      month: "Oktober",
+      date: "19",
+      day: "Sabtu",
+      hours: {
+        start: "09.00",
+        finish: "Selesai",
+      },
+    },
+    reception: {
+      year: "2025",
+      month: "Oktober",
+      date: "19",
+      day: "Minggu",
+      hours: {
+        start: "08.00",
+        finish: "Selesai",
+      },
+    },
+    address:
+      "Kp. Cinawala RT 02 RW 03 Desa Padajaya Kec Cikalongkulon Kab Cianjur",
+  },
+
+  link: {
+    calendar: "https://calendar.app.google/AV2pJ29gtFxpCefW7",
+    map: "https://maps.app.goo.gl/gfUEf6gVYNSq4vpM8",
+  },
+
+  galeri: [
+    {
+      id: 1,
+      image: "./src/assets/images/1.png",
+    },
+    {
+      id: 2,
+      image: "./src/assets/images/2.png",
+    },
+    {
+      id: 3,
+      image: "./src/assets/images/3.png",
+    },
+    {
+      id: 4,
+      image: "./src/assets/images/4.png",
+    },
+    {
+      id: 5,
+      image: "./src/assets/images/5.png",
+    },
+    {
+      id: 6,
+      image: "./src/assets/images/6.png",
+    },
+    {
+      id: 7,
+      image: "./src/assets/images/7.png",
+    },
+    {
+      id: 8,
+      image: "./src/assets/images/8.png",
+    },
+    {
+      id: 9,
+      image: "./src/assets/images/9.png",
+    },
+    {
+      id: 10,
+      image: "./src/assets/images/10.png",
+    },
+  ],
+
+  bank: [
+    {
+      id: 1,
+      name: "Ajeng Widia Sari",
+      icon: "./src/assets/images/bri.png",
+      rekening: "407401059664535",
+    },
+    {
+      id: 2,
+      name: "Ajeng Widia Sari",
+      icon: "./src/assets/images/dana.png",
+      rekening: "083850300391",
+    },
+  ],
+
+  audio: "./src/assets/audio/wedding.mp3",
+
+  api: "https://script.google.com/macros/s/AKfycbzjrSSFeDib-PJBQKc3pGw1Ya0X8PCqbB4KelqCi4txpUFrUQ0ZCiIJfYfZAiBcLXKV_Q/exec",
+
+  navbar: [
+    {
+      id: 1,
+      teks: "Home",
+      icon: "bx bxs-home-heart",
+      path: "#home",
+    },
+    {
+      id: 2,
+      teks: "Mempelai",
+      icon: "bx bxs-group",
+      path: "#bride",
+    },
+    {
+      id: 3,
+      teks: "Tanggal",
+      icon: "bx bxs-calendar-check",
+      path: "#time",
+    },
+    {
+      id: 4,
+      teks: "Galeri",
+      icon: "bx bxs-photo-album",
+      path: "#galeri",
+    },
+    {
+      id: 5,
+      teks: "Ucapan",
+      icon: "bx bxs-message-rounded-dots",
+      path: "#wishas",
+    },
+  ],
+};
